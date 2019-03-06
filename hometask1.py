754429ef5880e69ac793af47874b86fba8477bf2
Alex Popov <popov.alex.elf@gmail.com>
2019-03-06 09:01:52 +0200

    Update hometask1.py

--- hometask1.py
+++ hometask1.py
@@ -2,29 +2,29 @@
 import math
 
 def D(a, b, c):
-  return b*b - 4*a*c
+	return b*b - 4*a*c
 def Quad_eq(a, b, c):
-  
-  if a == 0:
-    if b == 0:
-      if c == 0:
-        return 'INF'
-      else:
-        return None
-    else:
-      x = -(c/b)
-      print(x)
-      return x
-  else:
-    d = D(a,b,c)
-    if d > 0:
-      d = math.sqrt(d)
-      x1 = (-b-d)/(2*a)
-      x2 = (-b+d)/(2*a)
-      return (x1, x2)
-    else:
-      if d == 0:
-        return None
-      else:
-        x = -b/(2*a)
-        return x
+	
+	if a == 0:
+		if b == 0:
+			if c == 0:
+				return 'INF'
+			else:
+				return None
+		else:
+			x = -(c/b)
+			print(x)
+			return x
+	else:
+		d = D(a,b,c)
+		if d > 0:
+			d = math.sqrt(d)
+			x1 = (-b-d)/(2*a)
+			x2 = (-b+d)/(2*a)
+			return (x1, x2)
+		else:
+			if d == 0:
+				x = -b/(2*a)
+				return x
+			else:
+				return None
